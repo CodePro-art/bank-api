39142e05188cd1c2f8aab4481c4a22a2990f4a40
Netanel Mazuz <netazuz@gmail.com>
2021-04-19 19:42:12 +0300

    update

--- src/client/components/Main/Main.js
+++ src/client/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from '../Button/Button';
+import Table from '../Table/Table';
 import './Main.css';
 
 export default class Main extends Component {
@@ -7,6 +8,7 @@ export default class Main extends Component {
   render() {
     return (
       <main className="main-container">
+        <Table/>
         <Button content={"click me"}/>
       </main>
     )
